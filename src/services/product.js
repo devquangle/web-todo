32ae67cab0bce94f592d8c5d66c814941efc8288
devquangle <devquangle8386@gmail.com>
2026-05-25 19:50:45 +0700

    feat: update login functionality and add product page with initial product data

---
+++ src/services/product.js
@@ -0,0 +1,33 @@
+
+
+
+
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    variants: [
+      {
+        skuId: 1,
+        quantity: 10,
+        price: 100000,
+        color: "Red",
+        size: "39",
+        skuCode: "SKU001",
+        status: true,
+      },
+      {
+        skuId: 1,
+        quantity: 10,
+        price: 100000,
+        color: "blue",
+        size: "40",
+        skuCode: "SKU001",
+        status: true,
+      },
+    ],
+
+    image: "https://via.placeholder.com/300x200?text=Product+1",
+    description: "This is a great product that you will love!",
+  },
+];
